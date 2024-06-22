75107b72e759a7ee7d6929a9b0aeb814c80d8d92
jihen <mlayehjihen@gmail.com>
2024-06-22 17:46:35 +0100

    Initial commit - add Angular Todo List App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { Todo } from './todo';
+import { AppService } from './app.service';
+import { CheckboxChangeEvent } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-primeng-todolist';
+export class AppComponent implements OnInit {
+  @ViewChild('todoTask') todoTask: any;
+  task=''
+  todos:Todo[]=[]
+  constructor(private appService:AppService ){}
+  
+  
+  ngOnInit(): void {
+    this.appService.getTodoList().subscribe(
+      Response=>this.todos=Response
+    )
+  }
+  getList():void{
+    this.appService.getTodoList().subscribe(
+      Response=>this.todos=Response
+    )
+  }
+  
+
+  updateTodo(e:CheckboxChangeEvent,todo:Todo){
+    this.appService.updateTodo({...todo,completed:e.checked})
+    .subscribe
+    (response=>console.log(response))
+
+  }
+  deleteTodo(e:unknown, todo:Todo){
+    this.appService.deleteTodo(todo.id).subscribe(
+      response=>this.getList()
+    )
+   
+   
+    }
+
+  
+  addTodo(){
+    this.appService.addTodo({task:this.task,completed:false}).subscribe(
+      Response=>{console.log(Response)
+        this.todoTask.reset()
+        this.appService.getTodoList().subscribe(
+          Response=>this.todos=Response
+        )
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    TableModule,
+    CheckboxModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
